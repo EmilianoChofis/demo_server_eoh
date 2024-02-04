ffc240b6fbc01b1b7f01730e3f091b139f7702d9
Isaacherna224 <118489250+Isaacherna224@users.noreply.github.com>
2024-02-04 15:07:23 -0600

    createPet

--- app.js
+++ app.js
@@ -21,4 +21,55 @@ const createClient = () => {
         });
 }
 
+createPet = () => {
+    const pets = [
+         {
+              name: 'Firulais',
+              type: 'Perro',
+              breed: 'Labrador',
+              owner_id: 1,
+              created_at: '2024-02-01 19:07:41'
+         },
+         {
+              name: 'Mishi',
+              type: 'Gato',
+              breed: 'Siamés',
+              owner_id: 2,
+              created_at: '2024-02-01 19:07:41'
+         },
+         {
+              name: 'Pirata',
+              type: 'Perico',
+              breed: 'Ara',
+              owner_id: 3,
+              created_at: '2024-02-01 19:07:41'
+         },
+            {
+                name: 'Bobby',
+                type: 'Perro',
+                breed: 'Pug',
+                owner_id: 4,
+                created_at: '2024-02-01 19:07:41'
+            },
+            {
+                name: 'Mota',
+                type: 'Gato',
+                breed: 'Egipcio',
+                owner_id: 5,
+                created_at: '2024-02-01 19:07:41'
+            }
+
+    ];
+
+    const random = Math.floor(Math.random() * pets.length);
+
+         const sql = `INSERT INTO pets (name, type, breed, owner_id, created_at) VALUES ('${pets[random].name}','${pets[random].type}','${pets[random].breed}',${pets[random].owner_id},'${pets[random].created_at}')`;
+         db.query(sql, (err, result) => {
+              if (err) throw err;
+              console.log('Mascota creada');
+         });
+
+}
+
 setInterval(createClient,5000);
+setInterval(createPet,5000);
